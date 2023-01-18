f1303a4bcea89568469d288ecea9aa226ab738a4
bjornandrip <bjornandrip@gmail.com>
2023-01-18 17:51:57 +0000

    added more grids and a dawg

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-
+import { Aclonica } from '@next/font/google'
+const aclonica = Aclonica({
+  subsets:['latin'],
+  weight:['400'],
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={aclonica.className}>
+      <Component {...pageProps} />
+    </main>
+
+  )
+  
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,37 +2,50 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='container'>
-      <div className='cell' id='one'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='cell'></div>
-      <div className='girl'>
-        <div className='blackDotRight' id='right'></div>
-        <div className='blackDotLeft' id='left'></div></div>
-    
-    </div>
-  )
+    <>
+  <Head>
+    <title>BAP.is</title>
+  </Head>
+  <div className='container'>
+  <div className='cell'></div>
+  <div className='cell'>
+    <div className='gridLinks'><Link href='/'>Home</Link></div>
+  </div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='cell'></div>
+  <div className='girl' id='girl'>
+    <div className='blackDotRight' id='right'></div>
+    <div className='blackDotLeft' id='left'></div></div>
+
+  </div>
+  </>
+  );
 }
